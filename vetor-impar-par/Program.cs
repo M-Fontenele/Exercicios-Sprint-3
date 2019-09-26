d490e66c1cc5c66d2b71e8a591787115e51fd12e
Matheus Fontenele <matheusfontenele8@gmail.com>
2019-09-26 17:09:53 -0300

    Exercicios

--- vetor-impar-par/Program.cs
+++ vetor-impar-par/Program.cs
@@ -6,26 +6,43 @@ namespace vetor_impar_par
     {
         static void Main(string[] args)
         {   
-            int[] numeros = new int[6];
+            int[] vetor = new int[6];
+            int par = 0;
+            int impar = 0;
 
-            for(int i = 0;i < 6; i++){
-                Console.WriteLine($"Digite o valor {i+1}");
-                numeros[i] = int.Parse(Console.ReadLine());
+            for(int cont = 0; cont < 6; cont++){
+                Console.Write($"Digite o {cont+1}º número: ");
+                vetor[cont] = int.Parse(Console.ReadLine());
             }
-            for(int n = 0 ; n < 6 ;n++){
-                int nump,numimp = 0;
-                if(((numeros[n]) % 2) == 0 ){
-                    if(n == 6){
-                        Console.WriteLine("");
-                    }
-                }
-                else{
-                    if(n == 6){
-                        Console.WriteLine("");
-                    }
+            foreach(int num in vetor){
+                if(num % 2 == 0){
+                    par++;
+                }else{
+                    impar++;
                 }
             }
+            Console.WriteLine($"Você tem {par} números pares e {impar} números impares");
+            // int[] numeros = new int[6];
 
+            // for(int i = 0;i < 6; i++){
+            //     Console.WriteLine($"Digite o valor {i+1}");
+            //     numeros[i] = int.Parse(Console.ReadLine());
+            // }
+            // for(int i = 0 ; i < 6 ;i++){
+            //     int nump,numimp = 0;
+            //     if(((numeros[i]) % 2) == 0 ){
+            //         nump=+ 1;
+            //         if(i == 6){
+            //             Console.WriteLine($"{nump}");
+            //         }
+            //     }
+            //     else{
+            //         numimp=+1;
+            //         if(i == 6){
+            //             Console.WriteLine($"{numimp}");
+            //         }
+            //     }
+            // }
         }
     }
 }
