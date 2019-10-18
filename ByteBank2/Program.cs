852bf0b2f84d3e35bdb7fcd385d7272c9bf11295
Matheus Fontenele <matheusfontenele8@gmail.com>
2019-10-18 16:04:54 -0300

    Exercicios-18/10/2019

---
+++ ByteBank2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ByteBank2.Models;
+
+namespace ByteBank2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente1 = new ContaCorrente(1,1, "Alexandre");
+            ContaCorrente contaCorrente2 = new ContaCorrente(1,2, "Cesar");
+            
+            ContaEspecial contaEspecial1 = new ContaEspecial(1,2, "Cesar");
+        }
+    }
+}
