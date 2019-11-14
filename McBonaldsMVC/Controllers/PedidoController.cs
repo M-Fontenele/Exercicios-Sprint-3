87a833e3c3f28250ee82d0179a60c3d41874ede9
Matheus Fontenele <matheusfontenele8@gmail.com>
2019-11-14 17:21:18 -0300

    Exercicios-14/11/2019

--- McBonaldsMVC/Controllers/PedidoController.cs
+++ McBonaldsMVC/Controllers/PedidoController.cs
@@ -1,4 +1,7 @@
+using System;
 using McBonaldsMVC.Models;
+using McBonaldsMVC.Repositories;
+using McBonaldsMVC.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,22 +9,33 @@ namespace McBonaldsMVC.Controllers
 {
     public class PedidoController : Controller
     {
+        PedidoRepository pedidoRepository = new PedidoRepository();
+        HamburguerRepository hamburguerRepository = new HamburguerRepository();
         public IActionResult Index()
         {
-            return View();
+
+            PedidoViewModel pvm = new PedidoViewModel();
+            pvm.Hamburgueres = hamburguerRepository.ObterTodos();
+
+            return View(pvm);
         }
 
         public IActionResult Registrar(IFormCollection form)
         {
+            ViewData["Action"] = "Pedido";
             Pedido pedido = new Pedido();
 
             Shake shake = new Shake();
             shake.Nome = form["shake"];
             shake.Preco = 0.0;
 
+            pedido.Shake = shake;
+
             Hamburguer hamburguer = new Hamburguer();
             hamburguer.Nome = form["hamburguer"];
             hamburguer.Preco = 0.0;
+
+            pedido.Hamburguer = hamburguer;
 
             Cliente cliente = new Cliente();
             cliente.Nome = form["nome"];
@@ -29,9 +43,20 @@ namespace McBonaldsMVC.Controllers
             cliente.Telefone = form["telefone"];
             cliente.Email = form["email"];
 
-            pedido.Shake = shake;
 
-            return View();
+            pedido.Cliente = cliente;
+
+            pedido.DataDoPedido = DateTime.Now;
+
+            pedido.PrecoTotal = 0.0;
+            
+            if(pedidoRepository.Inserir(pedido))
+            {
+                return View("Sucesso");
+            } else {
+                return View("Erro");
+            }
+
         }
     }
 }
